efb7508fd00608c3cc571b44408d0c0b34f30f49
Minh Le <100dayscode.minh@gmail.com>
2026-06-30 22:16:15 +1000

    add fastapi dependencies into pyprojecttoml

--- api/main.py
+++ api/main.py
@@ -1,6 +1,5 @@
 import datetime
 from fastapi import FastAPI, File, UploadFile
-from src.doc_processor.loader import load_document
 from src.services.ingestion import process_document
 from fastapi.responses import JSONResponse
 
@@ -17,8 +16,6 @@ def health_check():
 @app.post("/upload-document")
 async def upload_document_file(file: UploadFile = File(...)):
     
-    processed_document = await process_document(file)
-    print("--------")
-    print(processed_document)
-    print("--------")
-    return JSONResponse(content=processed_document)
+    processed_document_chunks = await process_document(file)
+   
+    return JSONResponse(content=processed_document_chunks)
